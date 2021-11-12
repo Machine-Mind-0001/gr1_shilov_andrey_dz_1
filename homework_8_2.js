dfad2fc53ab23f47de506341b4de71ab5cbc231b
Machine-Mind-0001 <93152055+Machine-Mind-0001@users.noreply.github.com>
2021-11-12 17:35:46 +0200

    Update homework_8_2.js

--- homework_8_2.js
+++ homework_8_2.js
@@ -3,8 +3,13 @@ div.className = "div1";
 div.style.background = "red";
 div.style.width = "192px";
 div.style.height = "128px";
-div.addEventListener('mouseover',log_div_size1);
-div.addEventListener('click',show_div_color1);
+div.addEventListener('mouseover',() => {
+  console.log(div.style.width);
+  console.log(div.style.height);
+});
+div.addEventListener('click',() => {
+  alert('Красный');
+});
 document.body.append(div);
 
 let div2 = document.createElement('div');
@@ -12,23 +17,11 @@ div2.className = "div2";
 div2.style.background = "green";
 div2.style.width = "256px";
 div2.style.height = "192px";
-div2.addEventListener('mouseover',log_div_size2);
-div2.addEventListener('click',show_div_color2);
-document.body.append(div2);
-
-function log_div_size1() {
-  console.log(div.style.width);
-  console.log(div.style.height);
-}
-function log_div_size2() {
+div2.addEventListener('mouseover',() => {
   console.log(div2.style.width);
   console.log(div2.style.height);
-}
-
-function show_div_color1() {
-  alert('Красный');
-};
-
-function show_div_color2() {
+});
+div2.addEventListener('click',() => {
   alert('Зеленый');
-};
+});
+document.body.append(div2);
